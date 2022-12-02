446364fe5dd972481a69b542a1dfbbb727e49cc2
Ned Batchelder <ned@nedbatchelder.com>
2022-12-02 17:48:22 -0500

    ay 2, part 2

--- day02.py
+++ day02.py
@@ -14,6 +14,8 @@ wins = {
     scissors: paper,
 }
 
+loses = {v:k for k,v in wins.items()}
+
 points = {
     rock: 1,
     paper: 2,
@@ -48,3 +50,30 @@ def test_part1():
 if __name__ == "__main__":
     score = part1_score(open("day02_input.txt"))
     print(f"Part 1: {score = }")
+
+
+def part2_score(guide_lines):
+    score = 0
+    for line in guide_lines:
+        him, me = line.split()
+        him = part1_translation[him]
+        if me == "X":
+            # lose
+            me = wins[him]
+        elif me == "Y":
+            # tie
+            me = him
+            score += 3
+        else:
+            # win
+            me = loses[him]
+            score += 6
+        score += points[me]
+    return score
+
+def test_part2():
+    assert part2_score(SAMPLE) == 12
+
+if __name__ == "__main__":
+    score = part2_score(open("day02_input.txt"))
+    print(f"Part 2: {score = }")
