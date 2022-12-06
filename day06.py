6b68daa8b32d1581e440bd23b45a1faf2f585a5c
Ned Batchelder <ned@nedbatchelder.com>
2022-12-06 05:34:44 -0500

    day 6, part 2

--- day06.py
+++ day06.py
@@ -13,12 +13,15 @@ SAMPLES = [
 ]
 
 
-def part1(buffer):
-    last4 = collections.deque(maxlen=4)
+def first_distinct(buffer, num):
+    recent = collections.deque(maxlen=num)
     for i, c in enumerate(buffer):
-        last4.append(c)
-        if len(last4) == 4 and len(set(last4)) == 4:
+        recent.append(c)
+        if len(recent) == num and len(set(recent)) == num:
             return i + 1
+
+def part1(buffer):
+    return first_distinct(buffer, 4)
 
 @pytest.mark.parametrize("buffer, answer", SAMPLES)
 def test_part1(buffer, answer):
@@ -28,3 +31,23 @@ def test_part1(buffer, answer):
 if __name__ == "__main__":
     data = open("day06_input.txt").read().strip()
     print(f"Part 1: {part1(data)}")
+
+
+SAMPLES2 = [
+    ("mjqjpqmgbljsphdztnvjfqwrcgsmlb", 19),
+    ("bvwbjplbgvbhsrlpgdmjqwftvncz", 23),
+    ("nppdvjthqldpwncqszvftbrmjlhg", 23),
+    ("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg", 29),
+    ("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw", 26),
+]
+
+def part2(buffer):
+    return first_distinct(buffer, 14)
+
+@pytest.mark.parametrize("buffer, answer", SAMPLES2)
+def test_part2(buffer, answer):
+    assert part2(buffer) == answer
+
+if __name__ == "__main__":
+    data = open("day06_input.txt").read().strip()
+    print(f"Part 2: {part2(data)}")
