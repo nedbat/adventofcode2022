bc23d2c2b2444a6c441f5f868d6a5cde8cc4025d
Ned Batchelder <ned@nedbatchelder.com>
2022-12-08 05:20:00 -0500

    day 8, part 2

--- day08.py
+++ day08.py
@@ -2,6 +2,8 @@
 
 import itertools
 
+import pytest
+
 SAMPLE = """\
 30373
 25512
@@ -17,9 +19,9 @@ def dimensions(trees):
     return len(trees[0]), len(trees)
 
 def sight_lines(x, y, w, h):
-    yield ((xx, y) for xx in range(x))
+    yield ((xx, y) for xx in range(x-1, -1, -1))
     yield ((xx, y) for xx in range(x+1, w))
-    yield ((x, yy) for yy in range(y))
+    yield ((x, yy) for yy in range(y-1, -1, -1))
     yield ((x, yy) for yy in range(y+1, h))
 
 def visible(x, y, trees):
@@ -44,3 +46,36 @@ def test_part1():
 if __name__ == "__main__":
     lines = open("day08_input.txt").read().splitlines()
     print(f"Part 1: {part1(lines)}")
+
+def scenic_score(x, y, trees):
+    w, h = dimensions(trees)
+    tree = trees[y][x]
+    score = 1
+    for sight_line in sight_lines(x, y, w, h):
+        visible = 0
+        for xx, yy in sight_line:
+            visible += 1
+            if trees[yy][xx] >= tree:
+                break
+        score *= visible
+    return score
+
+@pytest.mark.parametrize(
+    "x, y, score",
+    [(2, 1, 4), (2, 3, 8)]
+)
+def test_scenic_score(x, y, score):
+    trees = read_trees(SAMPLE)
+    assert scenic_score(x, y, trees) == score
+
+def part2(lines):
+    trees = read_trees(lines)
+    w, h = dimensions(trees)
+    return max(scenic_score(x, y, trees) for x, y in range2d(w, h))
+
+def test_part2():
+    assert part2(SAMPLE) == 8
+
+if __name__ == "__main__":
+    lines = open("day08_input.txt").read().splitlines()
+    print(f"Part 2: {part2(lines)}")
