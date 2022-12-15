5dd9cdd4b624abaa650146cf37d144fa959dd220
Ned Batchelder <ned@nedbatchelder.com>
2022-12-15 06:36:03 -0500

    day 15, part 2, but took 56s

--- day15.py
+++ day15.py
@@ -45,6 +45,23 @@ def union_ranges(r1, r2):
 def test_union_ranges(r1, r2, r3):
     assert union_ranges(r1, r2) == r3
 
+def intersect_ranges(r1, r2):
+    """Return the intersection of two ranges, None if empty."""
+    if r1.stop < r2.start or r2.stop < r1.start:
+        return None     # No overlap
+    return range(max(r1.start, r2.start), min(r1.stop, r2.stop))
+
+@pytest.mark.parametrize("r1, r2, r3", [
+    (range(0, 10), range(5, 15), range(5, 10)),
+    (range(5, 15), range(0, 10), range(5, 10)),
+    (range(0, 10), range(15, 25), None),
+    (range(0, 10), range(11, 25), None),
+    (range(0, 10), range(5, 8), range(5, 8)),
+    (range(10, 20), range(5, 15), range(10, 15)),
+])
+def test_intersect_ranges(r1, r2, r3):
+    assert intersect_ranges(r1, r2) == r3
+
 
 class Ranges:
     """A Ranges has a list of non-overlapping ranges."""
@@ -55,7 +72,7 @@ class Ranges:
         # ignores order, but we only use it for 1-element ranges in our tests...
         return self.ranges == other.ranges
 
-    def __add__(self, r):
+    def __or__(self, r):
         result = Ranges(r)
         for rs in self.ranges:
             overlapped = union_ranges(result.ranges[0], rs)
@@ -63,6 +80,14 @@ class Ranges:
                 result.ranges[0] = overlapped
             else:
                 result.ranges.append(rs)
+        return result
+
+    def __and__(self, r):
+        result = Ranges()
+        for rs in self.ranges:
+            inter = intersect_ranges(r, rs)
+            if inter is not None:
+                result.ranges.append(inter)
         return result
 
     def __len__(self):
@@ -76,7 +101,7 @@ def covered_positions(sensor_data, row_num):
         row_rad = radius - abs(sy - row_num)
         row_covered = range(sx - row_rad, sx + row_rad + 1)
         if row_rad >= 0:
-            covered += row_covered
+            covered |= row_covered
     return covered
 
 def test_covered_positions():
@@ -95,3 +120,25 @@ def test_part1():
 if __name__ == "__main__":
     text = open("day15_input.txt").read()
     print(f"Part 1: {part1(text, 2_000_000)}")
+
+def only_possible(text, size):
+    data = read_data(text)
+    for row_num in range(size):
+        covered = covered_positions(data, row_num)
+        covered &= range(size)
+        if len(covered.ranges) > 1:
+            return next(r.stop for r in covered.ranges if r.start == 0), row_num
+
+def test_only_possible():
+    assert only_possible(SAMPLE, 20) == (14, 11)
+
+def part2(text, size):
+    x, y = only_possible(text, size)
+    return x * 4_000_000 + y
+
+def test_part2():
+    assert part2(SAMPLE, 20) == 56_000_011
+
+if __name__ == "__main__":
+    text = open("day15_input.txt").read()
+    print(f"Part 2: {part2(text, 4_000_000)}")
