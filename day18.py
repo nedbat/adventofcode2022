40a3f0303d3a760d5e275505edc90bb2bfd6ce1b
Ned Batchelder <ned@nedbatchelder.com>
2022-12-18 08:10:29 -0500

    day 18, part 2

--- day18.py
+++ day18.py
@@ -49,3 +49,69 @@ def test_part1():
 if __name__ == "__main__":
     text = open("day18_input.txt").read()
     print(f"Part 1: {part1(text)}")
+
+
+def range2d_incl(la, ha, lb, hb):
+    for a in range(la, ha + 1):
+        for b in range(lb, hb + 1):
+            yield a, b
+
+def exterior(cubes):
+    lx = min(x for x, _, _ in cubes)
+    ly = min(y for _, y, _ in cubes)
+    lz = min(z for _, _, z in cubes)
+    hx = max(x for x, _, _ in cubes)
+    hy = max(y for _, y, _ in cubes)
+    hz = max(z for _, _, z in cubes)
+
+    # add a skin around the whole grid of outside cubes
+    outside = set()
+    candidates = set()
+    for y, z in range2d_incl(ly, hy, lz, hz):
+        outside.add((lx-1, y, z))
+        outside.add((hx+1, y, z))
+        candidates.add((lx, y, z))
+        candidates.add((hx, y, z))
+    for x, z in range2d_incl(lx, hx, lz, hz):
+        outside.add((x, ly-1, z))
+        outside.add((x, hy+1, z))
+        candidates.add((x, ly, z))
+        candidates.add((x, hy, z))
+    for x, y in range2d_incl(lx, hx, ly, hy):
+        outside.add((x, y, lz-1))
+        outside.add((x, y, hz+1))
+        candidates.add((x, y, lz))
+        candidates.add((x, y, hz))
+
+    while True:
+        next_candidates = set()
+        for cand in candidates:
+            if cand in cubes:
+                continue
+            if cand in outside:
+                continue
+            for ncand in neighbors(*cand):
+                if ncand in outside:
+                    outside.add(cand)
+                elif ncand not in cubes:
+                    next_candidates.add(ncand)
+        if not next_candidates:
+            break
+        candidates = next_candidates
+
+    exposed = 0
+    for pt in cubes:
+        for npt in neighbors(*pt):
+            if npt in outside:
+                exposed += 1
+    return exposed
+
+def part2(text):
+    return exterior(read_cubes(text))
+
+def test_part2():
+    assert part2(SAMPLE) == 58
+
+if __name__ == "__main__":
+    text = open("day18_input.txt").read()
+    print(f"Part 2: {part2(text)}")
